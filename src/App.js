d0ff88083eee504456e72964c7c3d71abe82a9b2
Adiljutt264 <adiljutt.264@gmail.com>
2024-01-07 20:17:58 +0500

    class based compenent this keyword counter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">React Life Cycle
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      name: "adil",
+      surname: "javed",
+      count: 0,
+    };
+    this.increment = this.increment.bind(this);
+  }
+  increment(){
+    this.setState((prev)=>{
+      return{
+        count: prev.count + 1,
+      }
+    })
+  }
+  render(){
+    return (
+      <div className="App">
+    <h2>{this.state.count}</h2>
+    <button onClick={this.increment}>+</button>
+      </div>
+    );
+}
 }
 
 export default App;
